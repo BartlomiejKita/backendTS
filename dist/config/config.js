c53599123f05059c106d001e7745810ef3635a5c
BartlomiejKita <bartlomiejpawelkita@gmail.com>
2023-02-02 13:23:23 +0100

    setup express
    add routes and logic for book model

--- dist/config/config.js
+++ dist/config/config.js
@@ -30,8 +30,6 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 const URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.zp20f30.mongodb.net/db-contacts?retryWrites=true&w=majority`;
 const PORT = process.env.PORT ? Number(process.env.PORT) : 9000;
-console.log(process.env.PORT);
-console.log(process.env.MONGO_USERNAME);
 exports.config = {
     mongo: {
         url: URI,
--- src/config/config.ts
+++ src/config/config.ts
@@ -6,8 +6,7 @@ const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 
 const URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.zp20f30.mongodb.net/db-contacts?retryWrites=true&w=majority`;
 const PORT = process.env.PORT ? Number(process.env.PORT) : 9000;
-console.log(process.env.PORT);
-console.log(process.env.MONGO_USERNAME);
+
 export const config = {
 	mongo: {
 		url: URI,
