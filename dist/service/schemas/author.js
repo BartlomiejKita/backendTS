bfa15666553c138968f34ff34d63192705325a31
Bartłomiej Kita <bartlomiejpawelkita@gmail.com>
2023-02-22 13:52:23 +0100

    Removing ignored files

--- dist/service/schemas/author.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// CREATE TABLE authors (
-//     author_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-//     author_name VARCHAR(100) NOT NULL,
-//     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-// );
